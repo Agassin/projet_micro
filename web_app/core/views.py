60f4fe87dc34dc0499748fdea7fe8b9bc33dfaa7
Agassin <129422340+Agassin@users.noreply.github.com>
2026-01-29 11:27:19 +0100

    ajout du site / pb de css a voir

--- web_app/core/views.py
+++ web_app/core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def eleve(request):
+    return render(request, 'eleve.html')
--- web_app/static_site/urls.py
+++ web_app/static_site/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('eleve/', views.eleve, name='eleve'),
 ]
