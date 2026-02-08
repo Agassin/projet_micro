28bb33fc26d3f086dc11bec99d9e3d8bbc69a4d8
Agassin <129422340+Agassin@users.noreply.github.com>
2026-02-08 19:12:31 +0100

    fix(#3): suppression du dossier inutile staticfiles

--- web_app/staticfiles/js/script.js
+++
@@ -1,7 +0,0 @@
-// Fonctionnalités générales du site
-console.log('Script global chargé');
-
-// Ajouter des événements listeneurs au besoin
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM chargé');
-});
